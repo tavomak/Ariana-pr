8cd0bf746b27fdb3b623574cd2da99de0f02f263
Gustavo Rivero <tavomak@gmail.com>
2017-02-14 15:48:24 -0400

    14 de febrero 2017

--- category-media-outlets.php
+++ category-media-outlets.php
@@ -1,76 +1,43 @@
 <?php get_template_part('includes/header'); ?>
 <div id="preloader">...</div><!-- loading -->
 <section class="container-fluid promo">
-    <?php if ( in_category( 'attract' ) ) : ?>
-       <div class="promo-content">
-            <img src="<?php bloginfo('template_directory'); ?>/asset/img/slide6.jpg" alt="" class="img-responsive">
-            <!--<div class="caption text-center">
-               <h1>Attract what you want</h1>
-            </div>-->
-       </div>
-    <?php elseif ( in_category( 'work-out' ) ) : ?>
-        <div class="promo-content">
-            <img src="<?php bloginfo('template_directory'); ?>/asset/img/slide2.jpg" alt="" class="img-responsive">
-            <!--<div class="caption text-center">
-                <h1>Eat but work out</h1>
-            </div>-->
-        </div>
-    <?php elseif ( in_category( 'fomo-events' ) ) : ?>
-       <div class="promo-content">
-            <img src="<?php bloginfo('template_directory'); ?>/asset/img/slide3.jpg" alt="" class="img-responsive">
-            <!--<div class="caption text-center">
-               <h1>Fomo-events</h1>
-           </div>-->
-       </div>
-    <?php elseif ( in_category( 'style' ) ) : ?>
-       <div class="promo-content">
-           <img src="<?php bloginfo('template_directory'); ?>/asset/img/slide5.jpg" alt="" class="img-responsive">
-           <!--<div class="caption text-center">
-               <h1>Style with attitude</h1>
-           </div>-->
-       </div>
-    <?php elseif ( in_category( 'travel-the-world' ) ) : ?>
-       <div class="promo-content">
-           <img src="<?php bloginfo('template_directory'); ?>/asset/img/slide4.jpg" alt="" class="img-responsive">
-           <!--<div class="caption text-center">
-               <h1>Take risks. Travel the world</h1>
-           </div>-->
-       </div>
-    <?php elseif ( in_category( 'media-outlets' ) ) : ?>
-       <div class="promo-content">
-           <img src="<?php bloginfo('template_directory'); ?>/asset/img/slide7.jpg" alt="Media Outlets" class="img-responsive">
-           <!--<div class="caption text-center">
-               <h1>Take risks. Travel the world</h1>
-           </div>-->
-       </div>
-    <?php endif; ?>
+   <div class="promo-content">
+       <img src="<?php bloginfo('template_directory'); ?>/asset/img/slide7.jpg" alt="Media Outlets" class="img-responsive">
+       <!--<div class="caption text-center">
+           <h1>Take risks. Travel the world</h1>
+       </div>-->
+   </div>
 </section>
+
 <section class="recent container-fluid">
-        <h1>Destacados</h1>
+
         <?php
-        $recents = new WP_Query(array(
+        $destacados = new WP_Query(array(
             'category_name'=>'destacado',
             'showposts'=>4
         ));
         ?>
-        <?php if ( $recents->have_posts() ) : while ( $recents->have_posts() ) : $recents->the_post(); ?>
+        <?php if ( $destacados->have_posts() ) : ?>
+        <h1>Destacados</h1>
+        <?php while ( $destacados->have_posts() ) : $destacados->the_post(); ?>
 
-            <div class="col-sm-6 col-md-4 col-lg-3 article post">
-                <div class="hovereffect">
-                   <img src="<?php the_post_thumbnail_url('medium_large'); ?>" class="">
-                    <a href="<?php the_permalink();?>" class="link">
-                        <div class="overlay">
-                            <h2><?php echo the_title();?></h2>
-                            <a class="info" href="<?php the_permalink();?>">Go There!</a>
-                        </div>
-                    </a>
-                </div>
+        <div class="col-sm-6 col-md-4 col-lg-3 article post">
+            <div class="hovereffect">
+               <img src="<?php the_post_thumbnail_url('medium_large'); ?>" class="">
+                <a href="<?php the_permalink();?>" class="link">
+                    <div class="overlay">
+                        <h2><?php echo the_title();?></h2>
+                        <a class="info" href="<?php the_permalink();?>">Go There!</a>
+                    </div>
+                </a>
             </div>
+        </div>
         <?php endwhile; else: ?>
-           <p class="text-center">soon we'll be found</p>
         <?php endif; ?>
         <?php wp_reset_query(); ?>
+
 </section><!-- Recientes -->
+
 <section class="container-fluid archive" id="content">
     <div class="row">
         <?php if ( have_posts() ) : ?>
@@ -92,16 +59,16 @@
         <?php endwhile; ?>
     </div><!-- /.row -->
 </section><!-- /.container -->
-            <div class="wrapper-pag"  id="#nav-below">
-        <?php if ( function_exists('a_pr_pagination') ) { a_pr_pagination(); } else if ( is_paged() ) { ?>
-                <ul class="pagination">
-                    <li class="older" id="nav-below-first"><?php next_posts_link('<i class="glyphicon glyphicon-arrow-left"></i> ' . __('Previous', 'a-pr')) ?></li>
-                    <li class="newer"><?php previous_posts_link(__('Next', 'a-pr') . ' <i class="glyphicon glyphicon-arrow-right"></i>') ?></li>
-                </ul>
-        <?php } ?>
-        <?php else: ?>
+    <div class="wrapper-pag"  id="#nav-below">
+    <?php if ( function_exists('a_pr_pagination') ) { a_pr_pagination(); } else if ( is_paged() ) { ?>
+        <ul class="pagination">
+            <li class="older" id="nav-below-first"><?php next_posts_link('<i class="glyphicon glyphicon-arrow-left"></i> ' . __('Previous', 'a-pr')) ?></li>
+            <li class="newer"><?php previous_posts_link(__('Next', 'a-pr') . ' <i class="glyphicon glyphicon-arrow-right"></i>') ?></li>
+        </ul>
+    <?php } ?>
+    <?php else: ?>
         <h1 class="text-center">soon we'll be found</h1>
-        <?php endif; ?>
-            </div>
+    <?php endif; ?>
+    </div>
 
 <?php get_template_part('includes/footer'); ?>
